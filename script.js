de42dc2905e0bc389e0d8e74acf3b18cc31b6a0d
ElSuarezz <manuelmachadosoares@hotmail.com>
2024-05-12 22:25:40 +0000

    add function to get computerchoice, add function to get humanchoice.

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 // write a function that randomly returns either "rock", "paper" or "scissors"
+    //get a random number from 0 to 2
+    // asign a string to each number
+
+
+//write a function that prompts the user to choose from "rock", "paper" or "scissors"    
+    //get a variable to prompt the question
+    //
 
 
 
+function getComputerChoice(randomchoice) {
+    const value = Math.random();
 
-function getComputerChoice() {
-    return  Math.floor(Math.random()*100)
-   
-   
+        if(value<=0.33) {
+            return "paper";
+
+        }else if (value>.033 && value>=0.66) {
+            return "scissors";
+
+        }else {
+            return "rock";
+        }
 }
-console.log(getComputerChoice())
+
+
+
+function getHumanChoice(insert) {
+    let choice = prompt("What is your choice? rock, paper or scissors?");
+        
+        if(choice="rock") {
+            return "rock";
+
+        }else if(choice="paper"){
+            return "paper";
+
+        }else if(choice="scissors"){
+            return "scissors";
+
+        }else{
+            return "not a valid input";
+        }
+}
+
